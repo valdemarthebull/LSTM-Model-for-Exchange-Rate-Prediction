fa1431f9a31996a442c780bc893e6afc9b1994f2
ValdemarG <95779321+valdemarthebull@users.noreply.github.com>
2023-05-03 23:48:53 +0200

    Add files via upload

---
+++ Model – BA/LSTMpy.py
@@ -0,0 +1,270 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error, median_absolute_error, mean_absolute_percentage_error
+import LSTMpy as lstm
+
+## Import nessesary packages
+import statsmodels.api as sm
+import statsmodels.stats.diagnostic
+from tabulate import tabulate
+import seaborn as sns
+from scipy import stats
+import warnings
+
+column = "SEKDKK"
+
+# Data normalization and sliding windows
+def sliding_windows(data, seq_length):
+    '''
+    Function for sliding windows
+    '''
+    
+    x = []
+    y = []
+
+    for i in range(len(data) - seq_length - 1):
+        _x = data[i:(i + seq_length)]
+        _y = data[i + seq_length]
+        x.append(_x)
+        y.append(_y)
+
+    return np.array(x), np.array(y)
+
+
+# Preprocessing function for the data + splitting into train and test and scaling
+
+
+def preprocessing(data, seq_length, train_ratio, val_ratio):
+    '''
+    Function for preprocessing data
+    '''
+    
+    ## Normalize data
+    sc = MinMaxScaler()
+    data_normalized = sc.fit_transform(data)
+    
+    ## Split data into train, validation and test
+    x, y = sliding_windows(data_normalized, seq_length)
+    
+    train_size = int(len(y) * train_ratio)
+    val_size = int(len(y) * val_ratio)
+    test_size = len(y) - train_size - val_size
+    
+    x, y = torch.Tensor(x), torch.Tensor(y)
+    
+    trainX, trainY = x[:train_size], y[:train_size]
+    valX, valY = x[train_size : train_size + val_size], y[train_size : train_size + val_size]
+    testX, testY = x[train_size + val_size :], y[train_size + val_size :]
+    
+    return trainX, trainY, valX, valY, testX, testY, sc, train_size, val_size, test_size, y, data_normalized
+
+
+
+## Create function for metrics
+
+
+def metrics(testY_plot, test_predict):
+    '''
+    Function for metrics
+    '''
+    
+    root_mean_squared_error = np.sqrt(mean_squared_error(testY_plot, test_predict))
+    
+    print("Test MSE:", mean_squared_error(testY_plot, test_predict))
+    print("Test R^2:", r2_score(testY_plot, test_predict))
+    print("Test MAE:", mean_absolute_error(testY_plot, test_predict))
+    print("Test Median AE:", median_absolute_error(testY_plot, test_predict))
+    print("Test MAPE:", mean_absolute_percentage_error(testY_plot, test_predict))
+    
+    ## output the metrics in a latex table using tabulate
+    table = [["R^2", r2_score(testY_plot, test_predict)], 
+            ["MSE", mean_squared_error(testY_plot, test_predict)],
+            ["MAE", mean_absolute_error(testY_plot, test_predict)],
+            ["MAPE", mean_absolute_percentage_error(testY_plot, test_predict)],
+            ["RMSE", root_mean_squared_error]]
+    
+    print(tabulate(table, tablefmt="latex", headers=["Metric", "Value"]))
+    
+    ## save the table as a latex file
+    with open(f"LSTM metrics for SEKDKK.tex", "w") as f:
+        f.write(tabulate(table, tablefmt="latex"))
+        
+
+
+def plot_curve200(train_losses, val_losses):
+    '''
+    Function for plotting the curve for 200 days
+    '''
+
+    plt.figure(figsize=(12, 8))
+    plt.plot(train_losses[:200], label="Training Loss")
+    plt.plot(val_losses[:200], label="Validation Loss")
+    plt.xlabel("Epochs")
+    plt.ylabel("Loss")
+    plt.title(f"Training and Validation Losses 200 Epochs – Learning Curve for SEKDKK")
+    plt.legend(loc="best", fontsize = 15)
+    plt.savefig(f'LSTM learning curve (100 Epochs) for SEKDKK')
+    plt.show()
+    
+    return plt
+
+
+
+#Plot the loss and validation loss on the y-axis and the number of epochs on the x-axis
+#PLot only for the first 100
+
+
+def plot_curve_all(train_losses, val_losses):
+    '''
+    Function for plotting the curve for all epochs
+    '''
+    plt.figure(figsize=(12, 8))
+    plt.plot(train_losses, label="Training Loss")
+    plt.plot(val_losses, label="Validation Loss")
+    plt.xlabel("Epochs")
+    plt.ylabel("Loss")
+    plt.title(f"Training and Validation Losses – Learning Curve for SEKDKK")
+    plt.legend(loc="best", fontsize = 15)
+    plt.savefig(f'LSTM learning curve for SEKDKK')
+    plt.show()
+    
+    
+## Define the function for "Directional Prediction Accuracy (DPA)"
+
+def DPA(testY_plot, test_predict):
+    '''
+    Function for calculating the directional prediction accuracy
+    '''
+    ## Define counter for the number of times the model predicts the correct direction of the exchange rate
+    counter = 0
+    correct_direction = 0
+    false_direction = 0
+
+    ## Loop through the test set and compare the predicted direction with the actual direction
+    for i in range(1, len(test_predict)):
+        if test_predict[i] > test_predict[i-1] and testY_plot[i] > testY_plot[i-1]:
+            counter += 1
+            correct_direction += 1
+        elif test_predict[i] < test_predict[i-1] and testY_plot[i] < testY_plot[i-1]:
+            counter += 1
+            correct_direction += 1
+        else:
+            counter -= 1
+            false_direction += 1
+        
+    ## Calculate the accuracy of the model
+    print("Counter", counter)
+    print("Total number of predictions:", len(test_predict))
+    print("Number of correct directions:", correct_direction)
+    print("Number of false directions:", false_direction)
+    print("Percentage of correct directions:", correct_direction / len(test_predict))
+    print("Percentage of false directions:", false_direction / len(test_predict))
+
+    ## output the metrics in a latex table using tabulate and a title 
+
+    table = [["Counter", counter],
+                ["Total number of predictions", len(test_predict)],
+                ["Number of correct directions", correct_direction],
+                ["Number of false directions", false_direction],
+                ["Percentage of correct directions", correct_direction / len(test_predict)],
+                ["Percentage of false directions", false_direction / len(test_predict)]]
+
+
+    print(tabulate(table, tablefmt="latex", headers=[f"LSTM {column}"]))
+    
+
+
+def model_eval(trainY_plot, train_predict, testY_plot, test_predict, train_size, val_size, test_size):
+    # Plotting
+    plt.figure(figsize=(15, 10))
+    plt.plot(trainY_plot, label="Training Data")
+    plt.plot(range(train_size, train_size + val_size), train_predict[train_size:], label="Validation Data")
+    plt.plot(range(train_size + val_size, train_size + val_size + len(testY_plot)), testY_plot, label="Test Data")
+    plt.plot(range(train_size + val_size, train_size + val_size + len(test_predict)), test_predict, label="Test Predictions")
+    plt.axvline(x=train_size, c="orange", linestyle="--", label="Training-Validation Cut-off")
+    plt.axvline(x=train_size + val_size, c="r", linestyle="--", label="Validation-Test Cut-off")
+    plt.legend(loc="best", fontsize="x-large")
+    plt.title(f"LSTM time-series prediction for SEKDKK", size=20)
+    plt.xlabel("Days", size=20)
+    plt.ylabel("SEK/DKK", size=20)
+    ## save the plot as a png file
+    plt.savefig(f'LSTM prediction for SEKDKK')
+    plt.show()
+
+    # Plot the last 100 values for the test set
+    plt.figure(figsize=(15, 10))
+    plt.plot(testY_plot[-100:], label="Test Data")
+    plt.plot(test_predict[-100:], label="Test Predictions")
+    plt.legend(loc="best", fontsize="x-large")
+    plt.title(f"LSTM time-series prediction for SEKDKK (Last 100 Days)" , size=20)
+    plt.xlabel("Days", size=20)
+    plt.ylabel("SEK/DKK", size=20)
+    ## save the plot as a png file
+    #plt.savefig(f'LSTM prediction for {column}')
+    plt.show()
+
+
+def jarque(testY_plot, test_predict):
+    '''
+    Function for calculating the Jarque-Bera test for normality of the residuals
+    '''
+    residuals =  testY_plot - test_predict
+
+    jb_test = stats.jarque_bera(residuals)
+    print("Jarque-Bera test for normality of the residuals: \n \n", jb_test)
+    
+    return jb_test
+
+
+
+def dist(testY_plot, test_predict):
+    # Plot distribution of data 
+    plt.figure(figsize=(20,10))
+    plt.title(f"LSTM – Distribution of model for {column}", size = 20)
+
+    residuals =  testY_plot - test_predict
+    mean = residuals.mean()
+    mean = float(mean)
+    
+    # remove warnings
+    warnings.filterwarnings('ignore')
+
+    ## add a vertical line at the mean
+    plt.axvline(mean, color='r', linestyle='dashed', linewidth=1, label = "Mean")
+
+    ## add true bell curve to the plot
+    sns.distplot(residuals, hist=True, kde=True, bins=30, color='b', hist_kws={'edgecolor':'black'}, kde_kws={'linewidth': 2}, label = "True Distribution")
+
+    plt.legend(fontsize = 15)
+    plt.show()
+    
+    
+def summary_table(testY_plot, test_predict, residuals):
+    """Function to create a summary table for the model"""
+    
+    mean = residuals.mean()
+    mean = float(mean)
+    
+    
+    
+    # define the summary table
+    summary_table = [["Mean", mean],
+                    ["Standard Error", np.std(residuals)],
+                    ["Skewness", stats.skew(residuals)],
+                    ["Kurtosis", stats.kurtosis(residuals)],
+                    ["Jarque-Bera test", stats.jarque_bera(residuals)[0]],
+                    ["p-value", stats.jarque_bera(residuals)[1]],
+                    ["Ljung-Box test", statsmodels.stats.diagnostic.acorr_ljungbox(residuals, lags=[5], return_df=True).iloc[0,0]],
+                    ["p-value", statsmodels.stats.diagnostic.acorr_ljungbox(residuals, lags=[5], return_df=True).iloc[0,1]],
+                    ["Heteroscedasticity", statsmodels.stats.diagnostic.het_arch(residuals)[0]],
+                    ["p-value", statsmodels.stats.diagnostic.het_arch(residuals)[1]],
+                    ["Normality", stats.normaltest(residuals)[0]],
+                    ["p-value", stats.normaltest(residuals)[1]]]
+    
+    
+    print(tabulate(summary_table, tablefmt="latex", headers=[f"Summary LSTM – {column}", "Value"]))
---
+++ Model – BA/arima.py
@@ -0,0 +1,473 @@
+## This script is used to generate the ARIMA model for the time series data
+# The script defines the functions used for the ARIMA jupyter file.
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import statsmodels.api as sm
+from statsmodels.tsa.stattools import adfuller
+import seaborn as sns
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+from sklearn.metrics import r2_score, mean_squared_error, mean_absolute_error, mean_absolute_percentage_error
+from sklearn.model_selection import TimeSeriesSplit
+from sklearn.model_selection import train_test_split
+from pmdarima import auto_arima
+from sklearn.linear_model import LinearRegression
+from statsmodels.stats.outliers_influence import variance_inflation_factor
+from sklearn.preprocessing import MinMaxScaler
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader, TensorDataset
+from tabulate import tabulate
+import LSTMpy as lstm
+import warnings
+import tqdm
+
+## set random seed
+np.random.seed(42)
+
+
+def test_stationarity(timeseries, column, when=''):
+    '''
+    This function is used to test the stationarity of the time series data.
+    The function uses the Augmented Dickey-Fuller test to test the stationarity of the data.
+    The function outputs a table of the results and saves the table as a latex table.
+    '''
+    
+    
+    result = adfuller(timeseries)
+    print(f"ADF Statistic: {result[0]}")
+    print(f"p-value: {result[1]}")
+    print("Critical Values:")
+    for key, value in result[4].items():
+        print(f"\t{key}: {value}")
+    if result[1] > 0.05:
+        print("The series is not stationary")
+   
+    else:
+        print("The series is stationary")
+    
+    ## use tabulate to output the results in a latex table
+    table = [["ADF Statistic", result[0]], ["p-value", result[1]]]
+    for key, value in result[4].items():
+        table.append([key, value])
+    
+    ## Add line to say if the series is stationary or not
+    if result[1] > 0.05:
+        table.append(["The series is not stationary as the p-value is greater than 0.05"])
+    else:
+        table.append(["The series is stationary as the p-value is less than 0.05"])
+    
+    print(tabulate(table, tablefmt="latex"))
+    
+    ## save the table a latex table to the path chosen above
+    with open(f"ARIMA {column}_stationarity({when}).tex", "w") as f:
+        f.write(tabulate(table, tablefmt="latex"))
+
+
+
+## differencing function and removing outliers
+
+def diff_outliers(data, column):
+    '''
+    This function is used to difference the data and remove the outliers.
+    The function outputs a table of the results and saves the table as a latex table.
+    '''
+    
+    # 1st order differencing to make the series stationary
+    diff_data = data.diff().dropna()
+    
+    # Remove outliers
+    diff_data = diff_data[diff_data[column] < 0.10]
+    diff_data = diff_data[diff_data[column] > -0.10]
+    
+    # table of the removed outliers
+    table = [["Removed outliers", len(data) - len(diff_data)], ["Total number of observations", len(data)]]
+    print(tabulate(table, tablefmt="latex"))
+    
+    ## save the table a latex table
+    with open(f"ARIMA {column}_outliers.tex", "w") as f:
+        f.write(tabulate(table, tablefmt="latex"))
+    
+    return diff_data
+
+
+
+def distribution(diff_data, column):
+    '''
+    This function is used to plot the distribution of the data.
+    The function outputs a plot of the distribution and saves the plot as a png.
+    '''
+    
+    # Calculate the mean of the data
+    mean = diff_data.mean()
+    mean = float(mean)
+    
+    # Plot distribution of data 
+    plt.figure(figsize=(20,10))
+    plt.title(f"Distribution of data for {column}", size = 20)
+    
+    ## add a vertical line at the mean
+    plt.axvline(mean, color='r', linestyle='dashed', linewidth=1, label = "Mean")
+    
+    ## add legend to plot
+    
+    
+    ## add true bell curve to the plot
+    sns.distplot(diff_data, hist=True, kde=True, bins=30, color='b', hist_kws={'edgecolor':'black'}, kde_kws={'linewidth': 2}, label = "True Distribution")
+    
+    plt.legend(fontsize = 15)
+    
+    ## Save fig to path
+    plt.savefig(f"ARIMA {column}_distribution.png")
+    
+    plt.show()
+
+
+
+def plot_all(data_train, data_test, diff_data, column, mean):
+    '''
+    This function is used to plot the train and test data.
+    The function outputs a plot of the train and test data and saves the plot as a png.
+    '''
+    
+    plt.figure(figsize=(20, 10))
+    plt.plot(data_train[-200:], label = 'Train Data (Last 200 days)')
+    plt.plot(data_test, label = 'Test Data')
+    plt.title('Train (Only 200 days) and Test data')
+    plt.axvline(x=data_train.index[-1],color='black', linestyle='--', label = 'Train/Test data cut-off')
+    plt.xticks(rotation=45)
+    plt.title(f'Train and Test data for {column}')
+    mean = diff_data.mean()
+    mean = float(mean)
+    print(mean)
+    plt.axhline(mean, color='r', linestyle='--', label = 'Data Mean')
+    plt.legend(loc='best', fontsize = 9)
+    plt.savefig(f"ARIMA {column}_train_test.png")
+    plt.show()
+
+
+def white_noise(data, lags, name=''):
+    """ 
+    Function to test if the data is white noise
+    """
+    ## run the test
+    test_results = sm.stats.acorr_ljungbox(data, lags=[lags], return_df=True)
+    
+    ## extract the p-value from the test results
+    p_value = test_results.iloc[0,1]
+    
+    ## print the results
+    print(f"The p-value from the white noise test is {p_value}")
+    
+    if p_value < 0.05:
+        print("The data is not white noise")
+    else:
+        print("The data is white noise")
+        
+    ## Output the test in a latex table using tabulate
+    table = [["Lag", "p-value"], [lags, p_value]]
+    print(tabulate(table, tablefmt="latex", headers = name))
+
+    
+    
+    ## return the p-value
+    return p_value
+
+
+## Initialising the model
+
+def model_initialise(data_train, data_test, column, mean, diff_data):
+    '''
+    This function is used to initialise the model.
+    The function outputs a plot of the train and test data and saves the plot as a png.
+    '''
+    
+    ## Create a matrix of zeros (Possible to use WLS aswell)
+    aic_matrix = np.zeros((10,10))
+    warnings.filterwarnings("ignore")
+    
+    
+    ## Loop through all the p and q values and fit the model and use progress bar to show progress from tqdm
+    for p in tqdm.tqdm(range(10)):
+        for q in range(10):
+            try:
+                model = sm.tsa.ARIMA(diff_data, order=(p,1,q)).fit()
+                aic_matrix[p,q] = model.aic
+            except:
+                pass
+    
+    ## Find the p and q values that give the lowest AIC value
+    p,q = np.unravel_index(np.argmin(aic_matrix), aic_matrix.shape)
+    
+    ## Print the p and q values
+    print(f"The p and q values are {p} and {q} respectively")
+    
+    ## Print the AIC value
+    print(f"The AIC value is {aic_matrix[p,q]}")
+    
+    ## Fit the model with the p and q values
+    model = sm.tsa.ARIMA(diff_data, order=(p,1,q)).fit()
+    
+    ## Print the model summary
+    print(model.summary())
+    
+    ## Save the model summary to a latex table
+    with open(f"ARIMA {column}_model_summary.tex", "w") as f:
+        f.write(model.summary().as_latex())
+    
+    return model, aic_matrix, p, q
+    
+
+## Function for heatmap of AIC values
+
+def heatmap(aic_matrix, column):
+    '''
+    This function is used to plot the heatmap of the AIC values.
+    The function outputs a plot of the heatmap and saves the plot as a png.
+    '''
+    # change size to 20 X 10 inches
+    plt.figure(figsize=(20,10))
+
+    ## make heat map only show the 10 best AIC values
+    sns.heatmap(aic_matrix, mask=(aic_matrix==0), annot=True, fmt='.2f', cmap = 'coolwarm')
+    plt.title(f'AIC Heatmap for different p and q values [Lower is better] – {column}', size = 20 )
+    plt.xlabel('q', size = 20)
+    plt.ylabel('p', size = 20)
+
+    ## Save fig to path
+    plt.savefig(f"ARIMA {column}_aic_heatmap.png")
+
+    plt.show()
+
+    print("The best p and q values are")
+    print(np.where(aic_matrix == np.min(aic_matrix)))
+
+
+## Plot the 30-day forecast and compare it to the actual values
+def plot_forecast_comparison(model, y_train, y_test, column, mean, steps=30):
+    forecast = model.predict(n_periods=steps)
+    forecast = pd.Series(forecast, index=y_test.index)
+    
+    plt.figure(figsize=(20, 10))
+    plt.plot(y_train[-100:], label='Observed (Training) (200 days)')
+    plt.plot(y_test, label='Observed (Test)')
+    plt.plot(forecast, label='Forecast')
+    plt.axvline(x=y_train.index[-1],color='black', linestyle='--', label = 'Train/Test data cut-off')
+    plt.title(f'Forecast vs Actual for {column}', size = 30)
+    plt.axhline(mean, color='r', linestyle='--', label = 'Data Mean')
+    plt.xlabel('Date')
+    plt.ylabel('SEKDKK', size = 20)
+    plt.legend(fontsize = 15)
+    
+    ## save fig to path
+    plt.savefig(f"ARIMA {column}_forecast.png")
+    
+    plt.show()
+    
+    return forecast
+    
+    
+    
+def get_metrics(y_true, y_pred):
+    r2 = r2_score(y_true, y_pred)
+    mse = mean_squared_error(y_true, y_pred)
+    mae = mean_absolute_error(y_true, y_pred)
+    mape = mean_absolute_percentage_error(y_true, y_pred)
+    rmse = np.sqrt(mse)
+    
+    ## output in a latex table
+    table = [["R2", r2], ["MSE", mse], ["MAE", mae], ["MAPE", mape], ["RMSE", rmse]]
+    print(tabulate(table, tablefmt="latex"))
+    
+    return table
+
+
+
+def direction_counter(forecast, data_test, column):
+
+    ## Define counter for the number of times the model predicts the correct direction of the exchange rate
+    counter = 0
+    counter_random = 0
+
+    # initiate random walk for comparison
+    correct_direction_random = 0
+    false_direction_random = 0
+
+    # initiate model direction counter
+    correct_direction = 0
+    false_direction = 0
+
+    ## initiate random walk for comparison
+    # set seed
+    np.random.seed(42)
+
+    # create random walk with forecast as index
+    random_walk = np.cumsum(np.random.normal(0, 1, len(forecast)))
+
+    # set index for random walk to be the same as the forecast
+    random_walk = pd.Series(random_walk, index=forecast.index)
+
+
+    # create a data frame with forecast, random walk and actual values
+    ## create a data frame with forecast, random walk and actual values
+    df = pd.DataFrame({'forecast': forecast, 'random_walk': random_walk, 'actual': data_test.squeeze()})
+
+
+    ## create loop to check if the forecast predicts the correct direction of the exchange rate when compared to the actual values
+    for i in range(len(df)):
+        if df['forecast'][i] > df['forecast'][i-1] and df['actual'][i] > df['actual'][i-1]:
+            counter += 1
+            correct_direction += 1
+        elif df['forecast'][i] < df['forecast'][i-1] and df['actual'][i] < df['actual'][i-1]:
+            counter += 1
+            correct_direction += 1
+        else:
+            counter += 1
+            false_direction += 1
+            
+        if df['random_walk'][i] > df['random_walk'][i-1] and df['actual'][i] > df['actual'][i-1]:
+            counter_random += 1
+            correct_direction_random += 1
+        elif df['random_walk'][i] < df['random_walk'][i-1] and df['actual'][i] < df['actual'][i-1]:
+            counter_random += 1
+            correct_direction_random += 1
+        else:
+            counter_random += 1
+            false_direction_random += 1
+
+
+    ## create latex table for the number of times the model predicts the correct direction of the exchange rate
+    table_model = [["Counter", counter],
+                ["Total number of predictions", len(forecast)],
+                ["Number of correct directions", correct_direction],
+                ["Number of false directions", false_direction],
+                ["Percentage of correct directions", correct_direction / len(forecast)],
+                ["Percentage of false directions", false_direction / len(forecast)]]
+
+    #print(tabulate(table_model, tablefmt="latex"))
+
+    table_random = [["Counter", counter_random],
+                ["Total number of predictions", len(forecast)],
+                ["Number of correct directions", correct_direction_random],
+                ["Number of false directions", false_direction_random],
+                ["Percentage of correct directions", correct_direction_random / len(forecast)],
+                ["Percentage of false directions", false_direction_random / len(forecast)]]
+
+    #print(tabulate(table_random, tablefmt="latex"))
+
+    ## a table which includes direction metrics for the random walk and the model with titles for random walk and model
+
+    pct_correct_arima = (correct_direction / (len(forecast) - 1))
+    pct_correct_random = (correct_direction_random / (len(forecast) - 1))
+    pct_wrong_arima = 1 - pct_correct_arima
+    pct_wrong_random = 1 - pct_correct_random
+
+
+    table_concat = [
+                ["Total number of predictions", len(forecast) - 1, len(forecast) - 1],
+                ["Number of correct directions", correct_direction, correct_direction_random],
+                ["Number of false directions", false_direction, false_direction_random],
+                ["Percentage of correct directions", "{:.2%}".format(pct_correct_arima), "{:.2%}".format(pct_correct_random)],
+                ["Percentage of false directions", "{:.2%}".format(pct_wrong_arima), "{:.2%}".format(pct_wrong_random)]]
+
+    print(tabulate(table_concat, tablefmt="latex", headers=[f"ARIMA {column}", "Random walk"]))
+            
+
+    ## save table to latex with, title headers and name
+    name = [f"ARIMA {column}", "Random walk"]
+
+    with open(f"ARIMA {column}_direction.tex", "w") as f:
+        f.write(tabulate(table_concat, tablefmt="latex", headers = name))
+
+    return random_walk, df, table_concat
+
+
+def rand(random_walk, forecast, data_test, column):
+    '''
+    Function to create a random walk and compare it to the actual values and the forecast
+    '''
+
+    
+    df = pd.DataFrame({'forecast': forecast, 'random_walk': random_walk, 'actual': data_test.squeeze()})
+    df = df.dropna()
+
+    # create table for last 30 days for random walk and print model in latex
+    table = df[-30:].describe()
+
+    # see if random walk can predict the direction of the exchange rate of the last 30 days for each day
+    counter = 0
+    correct_direction = 0
+    false_direction = 0
+
+    for i in range(len(df[-30:])):
+        if df['random_walk'][i] > df['random_walk'][i-1] and df['actual'][i] > df['actual'][i-1]:
+            counter += 1
+            correct_direction += 1
+        elif df['random_walk'][i] < df['random_walk'][i-1] and df['actual'][i] < df['actual'][i-1]:
+            counter += 1
+            correct_direction += 1
+        else:
+            counter += 1
+            false_direction += 1
+            
+    pct_correct_random = (correct_direction / (len(df[-30:])))
+    pct_wrong_random = 1 - pct_correct_random
+
+    table_random = [["Counter", counter],
+                ["Total number of predictions", len(df[-30:])],
+                ["Number of correct directions", correct_direction],
+                ["Number of false directions", false_direction],
+                ["Percentage of correct directions", "{:.2%}".format(pct_correct_random)],
+                ["Percentage of false directions", "{:.2%}".format(pct_wrong_random)]]
+
+
+    # print table with headers
+    print(tabulate(table_random, tablefmt="latex", headers=["Random walk"]))
+    
+
+
+def actual_forecat_random(random_walk, data_test, column, mean, model, y_test, steps = 30):
+    '''
+    Function to plot the actual values, the forecast and the random walk
+    '''
+    
+    
+    # Plot the last 30 values for the test set with random walk on second y axis
+    plt.figure(figsize=(20, 10))
+
+    # define two y axes
+    fig, ax1 = plt.subplots()
+
+    # make subplot bigger
+    fig.set_size_inches(20, 10)
+    
+    forecast = model.predict(n_periods=steps)
+    forecast = pd.Series(forecast, index=y_test.index)
+    
+
+    # plot test data and predictions on first y axis
+    ax1.plot(data_test[-30:], label='Actual', color = 'blue')
+    ax1.plot(forecast, label='Forecast', color = 'orange')
+    ax1.set_title(f'ARIMA – Forecast vs Actual for {column}', size = 30)
+    ax1.axhline(mean, color='r', linestyle='--', label = 'Data Mean')
+    ax1.set_xlabel('Date')
+    ax1.set_ylabel('SEKDKK', size = 20)
+
+    # plot random walk on second y axis
+    ax2 = ax1.twinx()
+    ax2.plot(random_walk, color='green', label='Random Walk')
+    ax2.set_ylabel('Random walk', size = 20)
+    ax2.tick_params(axis='y')
+
+
+    ## make legend for both y axes
+    lines_1, labels_1 = ax1.get_legend_handles_labels()
+    lines_2, labels_2 = ax2.get_legend_handles_labels()
+    ax2.legend(lines_1 + lines_2, labels_1 + labels_2, loc=0, fontsize = 20)
+
+    # save plot
+    plt.savefig(f"ARIMA {column}_forecast(30)_random.png")
+
+    plt.show()
